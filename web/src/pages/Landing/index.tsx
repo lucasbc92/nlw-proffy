799b89540bc4d6344d7a412246f20dd65aa4b27e
lucasbc92 <lbc92@hotmail.com>
2020-08-03 19:56:18 -0300

    Landing page

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,8 +1,45 @@
 import React from 'react';
 
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
+
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+
 const Landing = () => {
   return (
-    <div>Landing Page</div>
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+        <div className="logo-container">
+          <img src={logoImg} alt="Proffy" />
+          <h2>Your online study platform.</h2>
+        </div>
+
+        <img
+          src={landingImg}
+          alt="Study platform"
+          className="hero-image"
+        />
+
+        <div className="buttons-container">
+          <a href="/" className="study">
+            <img src={studyIcon} alt="Study" />
+            Study
+          </a>
+          <a href="/" className="give-lessons">
+            <img src={giveClassesIcon} alt="Give Lessons" />
+            Give Lessons
+          </a>
+        </div>
+
+        <span className="total-connections">
+          Total of 200 connections realized
+          <img src={purpleHeartIcon} alt="Purple Heart" />
+        </span>
+      </div>
+    </div>
   );
 };
 
