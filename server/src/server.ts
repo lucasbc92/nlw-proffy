81ee4555bbb317dcc73cf4b778c37d42f271577f
lucasbc92 <lbc92@hotmail.com>
2020-08-04 21:55:11 -0300

    Create class route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,12 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
+
 app.use(express.json());
+app.use(routes);
 
 const port = process.env.PORT || 3333;
-
-const users = [
-  {name: 'Lucas', age: 27},
-  {name: 'Diego', age: 25}
-]
-
-app.post('/', (request, response) => {
-  return response.json({message: "Hello World"});
-});
 
 app.listen(port, () => {
   console.log('Listening in port ' + port);
