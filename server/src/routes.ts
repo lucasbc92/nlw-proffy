96579e6e89870fb256d8c13919e1dcedfb4bc9b1
lucasbc92 <lbc92@hotmail.com>
2020-08-06 10:04:25 -0300

    Connections routes in back-end; initialized teacher form in front-end

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,67 +1,15 @@
 import express from 'express';
-import db from './database/connection';
-import convertHourToMinutes from './utils/convertHourToMinutes';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
+const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
 
-interface ScheduleItem {
-  week_day: number,
-  from: string,
-  to: string
-}
+routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.create);
 
-routes.post('/classes', async (request, response) => {
-  const {
-    name,
-    avatar,
-    whatsapp,
-    bio,
-    subject,
-    cost,
-    schedule
-  } = request.body;
-
-  const trx = await db.transaction();
-
-  try {
-    const insertedUsersId = await trx('users').insert({
-      name,
-      avatar,
-      whatsapp,
-      bio
-    })
-  
-    const user_id = insertedUsersId[0];
-  
-    const insertedClassesIds = await trx('classes').insert({
-      subject,
-      cost,
-      user_id,
-    });
-  
-    const class_id = insertedClassesIds[0];
-  
-    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-      return {
-        week_day: scheduleItem.week_day,
-        from: convertHourToMinutes(scheduleItem.from),
-        to: convertHourToMinutes(scheduleItem.to),
-        class_id
-      }
-    })
-  
-    await trx('class_schedule').insert(classSchedule);
-  
-    await trx.commit();
-  
-    return response.status(201).send();
-  } catch(err) {
-    trx.rollback();
-
-    return response.status(400).json({
-      error: 'Unexpected error while creating new class',
-    })
-  }
-});
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
 
 export default routes;
--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
-const TeacherForm = () => {
-  return (
-    <div id="page-teacher-form" className="container">
-      <PageHeader title="How amazing that you want to give classes." />
-    </div>
-  );
-};
+import './styles.css';
+
+const TeacherForm = () => (
+  <div id="page-teacher-form" className="container">
+    <PageHeader
+      title="How amazing that you want to give classes."
+      description="The first step is to fill this subscription form."
+    />
+
+    <main>
+      <fieldset>
+
+        <legend>Your data</legend>
+
+        <div className="input-block">
+          <label htmlFor="name">Full name</label>
+          <input type="text" id="name" />
+        </div>
+
+        <div className="input-block">
+          <label htmlFor="avatar">Avatar</label>
+          <input type="text" id="avatar" />
+        </div>
+
+        <div className="input-block">
+          <label htmlFor="whatsapp">WhatsApp</label>
+          <input type="text" id="whatsapp" />
+        </div>
+
+      </fieldset>
+    </main>
+  </div>
+);
 
 export default TeacherForm;
