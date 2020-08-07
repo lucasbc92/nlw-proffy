3046f50aa5d0650b86252c400ef6c81f2a5e10fb
lucasbc92 <lbc92@hotmail.com>
2020-08-07 18:52:52 -0300

    Schedule form in Teacher form without state

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -53,6 +53,33 @@ const TeacherForm = () => (
 
       </fieldset>
 
+      <fieldset>
+        <legend>
+          Schedule
+          <button type="button">
+            + New schedule
+          </button>
+        </legend>
+
+        <div className="schedule-item">
+          <Select
+            name="week-day"
+            label="Week Day"
+            options={[
+              { value: '0', label: 'Sunday' },
+              { value: '1', label: 'Monday' },
+              { value: '2', label: 'Tuesday' },
+              { value: '3', label: 'Wednesday' },
+              { value: '4', label: 'Thursday' },
+              { value: '5', label: 'Friday' },
+              { value: '6', label: 'Saturday' },
+            ]}
+          />
+          <Input name="from" label="From" type="time" />
+          <Input name="to" label="To" type="time" />
+        </div>
+      </fieldset>
+
       <footer>
         <p>
           <img src={warningIcon} alt="Warning" />
