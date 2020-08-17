d6cd063d53c565cd5eae0268eb7a2018168e0314
Lucas Bueno Cesário <lbc92@hotmail.com>
2020-08-16 23:27:57 -0300

    Page Header component

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
+import PageHeader from '../../components/PageHeader';
 
 import styles from './styles';
 
 const Favorites = () => {
-  return <View style={styles.container}>
-
-  </View>
+  return (
+    <View style={styles.container}>
+      <PageHeader title='My favorite Proffys' />
+    </View>
+  )
 }
 
 export default Favorites;
